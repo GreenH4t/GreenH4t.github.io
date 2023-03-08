8c5ebb1f76996cfab1da9055b03c0a48b5b14907
GreenH4t <106204025+GreenH4t@users.noreply.github.com>
2023-03-08 12:21:11 -0500

    Update math.js

--- math.js
+++ math.js
@@ -22,6 +22,62 @@ class math {
               defaultValue: '50'
             }
           }
+        },
+        {
+          opcode: 'notGrt',
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: '[ONE] ≯ [TWO]',
+          arguments: {
+            ONE: {
+              type: Scratch.ArgumentType.STRING
+            },
+            TWO: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: '50'
+            }
+          }
+        },
+        {
+          opcode: 'grtEq',
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: '[ONE] ≥ [TWO]',
+          arguments: {
+            ONE: {
+              type: Scratch.ArgumentType.STRING
+            },
+            TWO: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: '50'
+            }
+          }
+        },
+        {
+          opcode: 'notLess',
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: '[ONE] ≮ [TWO]',
+          arguments: {
+            ONE: {
+              type: Scratch.ArgumentType.STRING
+            },
+            TWO: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: '50'
+            }
+          }
+        },
+        {
+          opcode: 'lessEq',
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: '[ONE] ≤ [TWO]',
+          arguments: {
+            ONE: {
+              type: Scratch.ArgumentType.STRING
+            },
+            TWO: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: '50'
+            }
+          }
         }
       ]
     };
@@ -29,6 +85,19 @@ class math {
 
   notEqual(args) {
     return not args.ONE == args.TWO;
+  },
+  notGrt(args) {
+    return not args.ONE > args.TWO;
+  },
+  grtEq(args) {
+    return args.ONE >== args.TWO;
+  },
+  notLess(args) {
+    return not args.ONE < args.TWO;
+  },
+  lessEq(args) {
+    return args.ONE <== args.TWO;
   }
+}
 Scratch.extensions.register(new math());
 })(Scratch);
