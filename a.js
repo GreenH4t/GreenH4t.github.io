3f9882121d64271e0ae5036e159535fd83b9a728
GreenH4t <106204025+GreenH4t@users.noreply.github.com>
2023-03-08 19:24:30 -0500

    Update a.js

--- a.js
+++ a.js
@@ -1,21 +1,27 @@
-class HelloWorld {
-  getInfo() {
-    return {
-      id: 'helloworld',
-      name: 'It works!',
-      blocks: [
-        {
-          opcode: 'hello',
-          blockType: Scratch.BlockType.REPORTER,
-          text: 'Hello!'
-        }
-      ]
-    };
+(function(Scratch) {
+  'use strict';
+
+  if (!Scratch.extensions.unsandboxed) {
+    throw new Error('This Hello World example must run unsandboxed');
   }
 
-  hello() {
-    return 'World!';
+  class HelloWorld {
+    getInfo() {
+      return {
+        id: 'helloworldunsandboxed',
+        name: 'Unsandboxed Hello World',
+        blocks: [
+          {
+            opcode: 'hello',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'Hello!'
+          }
+        ]
+      };
+    }
+    hello() {
+      return 'World!';
+    }
   }
-}
-
-Scratch.extensions.register(new HelloWorld());
+  Scratch.extensions.register(new HelloWorld());
+})(Scratch);
