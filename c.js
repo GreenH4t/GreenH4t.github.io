ed9369643571d239f6c907bac27456b22dcf23e2
GreenH4t <106204025+GreenH4t@users.noreply.github.com>
2023-03-08 19:53:14 -0500

    Create c.js

---
+++ c.js
@@ -0,0 +1,46 @@
+class StrictEqualityExtension {
+  getInfo() {
+    return {
+      id: 'strictequalityexample',
+      name: 'Strict Equality',
+      blocks: [
+        {
+          opcode: 'strictlyEquals',
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: '[ONE] strictly equals [TWO]',
+          arguments: {
+            ONE: {
+              type: Scratch.ArgumentType.STRING
+            },
+            TWO: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'Second value'
+            }
+          }
+        },
+        {
+          opcode: 'grtEq',
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: '[ONE] ≥ [TWO]',
+          arguments: {
+            ONE: {
+              type: Scratch.ArgumentType.STRING
+            },
+            TWO: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'Second value'
+            }
+          }
+        }
+      ]
+    };
+  }
+
+  strictlyEquals(args) {
+    return args.ONE === args.TWO;
+  }
+  grtEq(args) {
+    return args.ONE >== args.TWO;
+  }
+}
+Scratch.extensions.register(new StrictEqualityExtension());
